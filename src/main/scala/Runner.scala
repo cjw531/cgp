e277052647da48e5a2749ec5f98cc11bf0bbc85f
Jiwon Choi <cjwon531@gmail.com>
2021-05-10 00:03:51 -0400

    fixed true function: x^2 is not equivalent to x * x

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -30,28 +30,28 @@ object Runner extends App {
 
   // define true function to evaluate
   def func(x: Int): Int = {
-    return x^2 + 2*x + 1
+    return (x * x) + (2 * x) + 1 // x^2 + 2x + 1
   }
 
   // Evaluate sample of points on true function
   val true_values = ListBuffer[Int]()
-  for (point <- this.sample_points) {
+  for (point <- sample_points) {
     true_values += func(point)
   }
 
   print("True values: ")
-  println(true_values)
+  println(true_values )
 
   // initially create CGPs and find the best one
-  for (i <- 0 to num_cgps_to_evaluate) {
+  for (i <- 0 to num_cgps_to_evaluate) { // create multiple cgps for each agent
     var CGP = new Cgp(1, num_output, lv_back, num_row, num_col, function_options)
     CGP.create_cgp()
 
     var preds = CGP.decode_cgp(sample_points)
     print("Predictions: ")
     println(preds)
-    // squared difference
 
+    // squared difference
     // how are preds
     var diff = ListBuffer[BigInt]()
     for (i <- 0 to preds.length-1) {
@@ -61,13 +61,16 @@ object Runner extends App {
     for (i <- 0 to diff.length-1) {
       diff_squared += diff(i).pow(2)
     }
+
     print("Initial difference taken: ")
     println(diff)
     print("Squared difference: ")
     println(diff_squared)
+
     CGP.evaluation_score = diff_squared.sum
     print("Evaluation Score: ")
     println(CGP.evaluation_score)
+
     if (i == 0) {
       best_cgp_idx = i
       lowest_cgp_metric_val = CGP.evaluation_score
